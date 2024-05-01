a9fbecb0ea2ce702eaae1200babc4705b8b2e56c
Tydy1410 <thienntse171341@fpt.edu.vn>
2024-05-01 11:38:39 +0700

    nhanh bo may`

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline ">
+        Hello world! lok hard qua
+      </h1>
+    
+      <h1 className="bg-black w-[500px] h-[500px] ">Lok hard qua</h1>
+    </>
   )
 }
